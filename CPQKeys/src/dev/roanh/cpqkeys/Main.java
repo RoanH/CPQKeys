1532f3ea1c58e4bd38cf37b51edc321cc52f64b6
Roan Hofland <roan@roanh.dev>
2022-05-26 17:48:36 +0200

    Small configuration fixes & test

--- CPQKeys/src/dev/roanh/cpqkeys/Main.java
+++ CPQKeys/src/dev/roanh/cpqkeys/Main.java
@@ -1,5 +1,16 @@
 package dev.roanh.cpqkeys;
 
+import java.nio.file.Paths;
+
+import dev.roanh.cpqkeys.algo.Nauty;
+
 public class Main{
 
+	public static void main(String[] args){
+		System.out.println("start");
+		
+		System.load(Paths.get("./native/libnauty.so").toAbsolutePath().toString());
+		
+		System.out.println("test: " + Nauty.test(10));
+	}
 }
