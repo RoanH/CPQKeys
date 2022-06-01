f8bbae59a212bf748b09d23b5eb324055b07c3b8
Roan Hofland <roan@roanh.dev>
2022-06-01 02:22:44 +0200

    Implement data conversion

--- CPQKeys/bindings/nishe/dev_roanh_cpqkeys_algo_Nishe.cpp
+++ CPQKeys/bindings/nishe/dev_roanh_cpqkeys_algo_Nishe.cpp
@@ -2,6 +2,7 @@
 #include <nishe/PartitionNest.h>
 #include <nishe/Refiner-inl.h>
 #include <nishe/DirectedGraph.h>
+#include <sstream>
 
 using namespace nishe;
 
@@ -9,7 +10,41 @@ JNIEXPORT jint JNICALL Java_dev_roanh_cpqkeys_algo_Nishe_test(JNIEnv* env, jclas
 	return 2 * num;
 }
 
-JNIEXPORT void JNICALL Java_dev_roanh_cpqkeys_algo_Nishe_computeCanon(JNIEnv*, jclass, jobjectArray, jintArray){
+JNIEXPORT void JNICALL Java_dev_roanh_cpqkeys_algo_Nishe_computeCanon(JNIEnv* env, jclass obj, jobjectArray adj, jintArray colors){
+	//construct pi
+	PartitionNest pi;
+
+	jsize len = env->GetArrayLength(adj);
+	pi.unit(len);//TODO maybe not required
+
+	jint* colorData = env->GetIntArrayElements(colors, 0);
+	for(int i = 0; i < len; i++){
+		if(colorData[i] < 0){
+			pi.enqueue_new_index(-colorData[i] - 1);
+			pi.advance_level();
+			pi.commit_pending_indices();
+		}else{
+			pi.enqueue_new_index(colorData[i] - 1);
+		}
+	}
+	env->ReleaseIntArrayElements(colors, colorData, 0);
+
+	//construct graph
+	DirectedGraph graph;
+	for(int u = 0; u < len; u++){
+		jintArray row = (jintArray)(env->GetObjectArrayElement(adj, u));
+		jsize rlen = env->GetArrayLength(row);
+		jint* elem = env->GetIntArrayElements(row, 0);
+
+		for(int i = 0; i < rlen; i++){
+			graph.add_arc(u, elem[i]);
+		}
+
+		env->ReleaseIntArrayElements(row, elem, 0);
+	}
+
+	//verify
+	std::cout << "pi: " << pi << std::endl;
 
 }
 
