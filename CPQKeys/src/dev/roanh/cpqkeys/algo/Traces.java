6dafc88dedeac8f53cd8b937de711dad1efcb96b
Roan Hofland <roan@roanh.dev>
2022-06-07 19:38:54 +0200

    Actually add traces test case

--- CPQKeys/src/dev/roanh/cpqkeys/algo/Traces.java
+++ CPQKeys/src/dev/roanh/cpqkeys/algo/Traces.java
@@ -7,7 +7,23 @@ public class Traces{
 	
 	
 	
-	
+	public static void test(){
+		int[][] graph = new int[][]{
+			{1, 2, 3, 4, 5},
+			{0, 1, 2, 3, 4, 5},
+			{0, 1},
+			{0, 1},
+			{0, 1},
+			{0, 1}
+		};
+		
+		//+1 it all so negation can be used to denote range ends
+		int[] colors = new int[]{1, -3, 2, 4, -5, -6};
+		
+		long[] times = computeCanon(graph, colors);
+		System.out.println("construct time: " + times[0]);
+		System.out.println("runtime: " + times[1]);
+	}
 	
 	//NOTE: Traces REQUIRES an undirected graph so make sure to add all edges in both directions
 	private static native long[] computeCanon(int[][] adj, int[] colors);
