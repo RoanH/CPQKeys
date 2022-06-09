1683fd3acbe3f2ebc132dec407de8d1b514f2ce2
Roan Hofland <roan@roanh.dev>
2022-06-09 18:13:59 +0200

    Bliss template

---
+++ CPQKeys/bindings/bliss/dev_roanh_cpqkeys_algo_Bliss.cpp
@@ -0,0 +1,27 @@
+#include <dev_roanh_cpqkeys_algo_Bliss.h>
+#include <nishe/graph.hh>
+#include <chrono>
+
+using namespace std::chrono;
+
+JNIEXPORT jlongArray JNICALL Java_dev_roanh_cpqkeys_algo_Bliss_computeCanon(JNIEnv* env, jclass obj, jintArray edges, jintArray colors){
+	steady_clock::time_point start_time = steady_clock::now();
+
+	//TODO prep
+
+	steady_clock::time_point mid_time = steady_clock::now();
+
+	//TODO canon
+
+	steady_clock::time_point end_time = steady_clock::now();
+
+	//return times
+	jlongArray result = env->NewLongArray(2);
+
+	jlong data[2];
+	data[0] = duration_cast<nanoseconds>(mid_time - start_time).count();
+	data[1] = duration_cast<nanoseconds>(end_time - mid_time).count();
+	env->SetLongArrayRegion(result, 0, 2, data);
+
+	return result;
+}
