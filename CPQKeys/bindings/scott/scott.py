e18b98d73a3533d48d6b4d8e943eb9d879c443ca
Roan Hofland <roan@roanh.dev>
2022-06-11 01:13:44 +0200

    Add some notes on Scott

---
+++ CPQKeys/bindings/scott/scott.py
@@ -0,0 +1,33 @@
+import scott.scott as st
+
+graph = st.structs.graph.Graph()
+n1 = st.structs.node.Node("1", "C")
+n2 = st.structs.node.Node("2", "O")
+n3 = st.structs.node.Node("3", "H")
+n4 = st.structs.node.Node("4", "H")
+
+e1 = st.structs.edge.Edge("1", n1, n2, modality=2)
+e2 = st.structs.edge.Edge("2", n1, n3, modality="test")
+e4 = st.structs.edge.Edge("4", n1, n3, modality="testb")
+e3 = st.structs.edge.Edge("3", n1, n4, directed=False)
+e5 = st.structs.edge.Edge("5", n1, n1)
+
+
+graph.add_node(n1)
+graph.add_nodes([n2, n3, n4])
+graph.add_edge(e1)
+graph.add_edge(e2)
+graph.add_edge(e3)
+graph.add_edge(e4)
+graph.add_edge(e5)
+
+
+#print(n1)
+#print(e1)
+print(graph)
+
+print(str(st.canonize.to_cgraph(graph)))
+
+# modality = edge labels?
+# directed edges are supported after all? or is this experimental?
+# parallel edges are supported
