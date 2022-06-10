08d3cffa764803d26d6e82d1bd56b4ce8f3c9068
Roan Hofland <roan@roanh.dev>
2022-06-10 02:52:17 +0200

    Properly apply the GPL
    Signed-off-by: Roan Hofland <roan@roanh.dev>

--- CPQKeys/bindings/nauty_traces/core.c
+++ CPQKeys/bindings/nauty_traces/core.c
@@ -1,3 +1,21 @@
+/*
+ * CPQKeys: An evaluation of various graph canonization algorithms.
+ * Copyright (C) 2022  Roan Hofland (roan@roanh.dev).  All rights reserved.
+ * GitHub Repository: https://github.com/RoanH/CPQKeys
+ *
+ * CPQKeys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CPQKeys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <core.h>
 
 /**
