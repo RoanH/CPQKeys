afea9bbc9f73de0472b2b7fdcfdce19054e79455
Roan Hofland <roan@roanh.dev>
2022-06-21 02:21:18 +0200

    Add a fair bit of general documentation

--- CPQKeys/src/dev/roanh/cpqkeys/Main.java
+++ CPQKeys/src/dev/roanh/cpqkeys/Main.java
@@ -39,12 +39,66 @@ import dev.roanh.cpqkeys.algo.Scott;
 import dev.roanh.cpqkeys.algo.Traces;
 import dev.roanh.gmark.util.Util;
 
+/**
+ * Main class for the CPQ Keys canonization
+ * algorithm evaluation framework.
+ * @author Roan
+ */
 public class Main{
+	/**
+	 * Executor used to run the algorithms.
+	 */
 	private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+	/**
+	 * The maximum amount of time in nanoseconds that an algorithm is allowed
+	 * to spend evaluating a complete data set. If the algorithms needs more
+	 * time to run on a data set then it will not be given a larger data set.
+	 * Conversely, if the algorithm manages to finish processing the whole
+	 * data set within this time limit, then it is given a larger data set
+	 * to run on next.
+	 */
 	private static final long MAX_RUNTIME = TimeUnit.SECONDS.toNanos(10);
+	/**
+	 * Number of graphs in a data set.
+	 */
+	private static final int DATASET_SIZE = 10;
+	/**
+	 * Number of rule applications to use to generate the initial data set.
+	 * @see GraphDataSet#fromCPQ(int, int, int)
+	 */
+	private static final int MIN_RULES = 4;
+	/**
+	 * Maximum number of rule applications to use to generate a data set.
+	 * Algorithms will not be given a larger data set to work with even
+	 * if they manage to complete their work on a data set of this size
+	 * within the set time limit.
+	 * @see GraphDataSet#fromCPQ(int, int, int)
+	 */
+	private static final int MAX_RULES = 8196 * 2 * 2;
+	/**
+	 * Increase in data set size after an algorithm processes a data set
+	 * within the set time limit. Expressed as the number of rule applications.
+	 * @see GraphDataSet#fromCPQ(int, int, int)
+	 */
+	private static final int RULE_GROWTH_FACTOR = 2;
+	/**
+	 * Number of labels to use for the data set.
+	 * @see GraphDataSet#fromCPQ(int, int, int)
+	 */
+	private static final int LABELS = 5;
+	/**
+	 * Random seed to use to generate data sets.
+	 */
+	private static final int SEED = 1234;
+	/**
+	 * Command to start a Python process on the host system.
+	 */
 	public static final String PYTHON_COMMAND = findPython();
+	/**
+	 * List of algorithms to evaluate.
+	 */
 	public static final List<Algorithm> algorithms = Arrays.asList(
-		Scott.DIRECTED,
+		Scott.DIRECTED,//known to have issues with certain inputs
 		Scott.UNDIRECTED,
 		Nishe.INSTANCE,
 		Nauty.SPARSE,
@@ -53,11 +107,14 @@ public class Main{
 		Bliss.INSTANCE
 	);
 
+	/**
+	 * Starts the algorithm evaluation process.
+	 * @param args No valid commandline arguments.
+	 */
 	public static void main(String[] args){
 		try{
 			loadNatives();
 		}catch(IOException | UnsatisfiedLinkError e){
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -69,11 +126,11 @@ public class Main{
 	}
 	
 	private static final void evaluateAlgorithm(Algorithm algo){
-		Util.setRandomSeed(1234);
+		Util.setRandomSeed(SEED);
 		
 		Future<ReportSummaryStatistics> task = null;
-		for(int i = 4; i <= 8196 * 2 * 2; i *= 2){
-			GraphDataSet data = GraphDataSet.fromCPQ(10, i, 5);
+		for(int i = MIN_RULES; i <= MAX_RULES; i *= RULE_GROWTH_FACTOR){
+			GraphDataSet data = GraphDataSet.fromCPQ(DATASET_SIZE, i, LABELS);
 			data.print();
 			
 			task = executor.submit(()->new ReportSummaryStatistics(algo, data));
@@ -103,6 +160,14 @@ public class Main{
 		System.out.println("Not generating the next dataset as this would be very expensive...");
 	}
 	
+	/**
+	 * Loads the compiled JNI libraries required for certain algorithm implementations.
+	 * Loads JNI dynamic libraries from the 'native' folder with libraries inside the
+	 * 'native/lib' being loaded first so they can be used as dependencies for other
+	 * libraries without causing errors.
+	 * @throws IOException When an IOException occurs.
+	 * @throws UnsatisfiedLinkError When loading a native library fails.
+	 */
 	private static final void loadNatives() throws IOException, UnsatisfiedLinkError{
 		for(Path lib : Files.newDirectoryStream(Paths.get("native").resolve("lib"))){
 			System.out.println("Loading native core library: " + lib.getFileName());
@@ -115,6 +180,12 @@ public class Main{
 		}
 	}
 	
+	/**
+	 * Finds a Python 3 installation on the host system. This will
+	 * typically return the <code>python3</code> command unless an
+	 * anaconda 3 installation is detected.
+	 * @return The Python executable.
+	 */
 	private static final String findPython(){
 		if(System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("windows")){
 			Path anaconda = Paths.get(System.getProperty("user.home")).resolve("anaconda3").resolve("python.exe");
